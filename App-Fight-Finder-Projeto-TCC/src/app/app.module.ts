59ef1c1330738db87248e89993a238fe23c91d84
Letícia Zalasik <leticiazalasik@gmail.com>
2024-05-30 17:36:50 -0300

    :construction: Feat (app-component): mudanças no HTML e CSS).

--- App-Fight-Finder-Projeto-TCC/src/app/app.module.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LutadorComponent } from './components/lutador/lutador.component';
 import { LutadorFormComponent } from './components/lutador-form/lutador-form.component';
+import { LutadorListComponent } from './components/lutador-list/lutador-list.component';
+
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
   declarations: [
     AppComponent,
     LutadorComponent,
-    LutadorFormComponent
+    LutadorFormComponent, 
+    LutadorListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +29,8 @@ import { LutadorFormComponent } from './components/lutador-form/lutador-form.com
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
+
+  
 })
 export class AppModule { }
 
