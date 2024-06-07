fa76f694f2f00f856d18bbc49b60c60e617bdbdf
Letícia Zalasik <leticiazalasik@gmail.com>
2024-06-07 09:52:29 -0300

    :sparkles: Feat (app-component): .

--- App-Fight-Finder-Projeto-TCC/src/app/app.module.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { LutadorComponent } from './components/lutador/lutador.component';
 import { LutadorFormComponent } from './components/lutador-form/lutador-form.component';
 import { LutadorListComponent } from './components/lutador-list/lutador-list.component';
 import { LutadorEditComponent } from './components/lutador-edit/lutador-edit.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
@@ -19,7 +21,7 @@ import { LutadorEditComponent } from './components/lutador-edit/lutador-edit.com
     LutadorComponent,
     LutadorFormComponent, 
     LutadorListComponent, 
-    LutadorEditComponent
+    LutadorEditComponent, FooterComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
