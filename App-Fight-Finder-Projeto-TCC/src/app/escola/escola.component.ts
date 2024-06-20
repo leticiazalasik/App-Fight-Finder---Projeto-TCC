eb1b0e31748b3063e0785154cc866ccc229c1fca
Letícia Zalasik <leticiazalasik@gmail.com>
2024-06-20 17:42:45 -0300

    :construction: Feat (tela-login): Conectando escola com tela-login.

--- App-Fight-Finder-Projeto-TCC/src/app/escola/escola.component.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/escola/escola.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-escola',
   templateUrl: './escola.component.html',
   styleUrl: './escola.component.css'
 })
-export class EscolaComponent {
+export class Escola {
 
+  // escola.model.ts
+
+  id: number;
+  nome: string;
+  email: string;
+  senha: string;
+  foto: string;
+  treinadores: string;
+  alunos: string;
+  aulas: number;
+
+      constructor( @Inject(Number) id: number, nome: string, email:string, senha:string, foto:string, treinadores: string, alunos: string, aulas:number) {
+        this.id = id;
+        this.nome = nome;
+        this.email=email;
+        this.senha=senha;
+        this.foto=foto;
+        this.treinadores = treinadores;
+        this.alunos = alunos;
+        this.aulas=aulas; 
+    }
 }
+
+@Component({
+    selector: 'app-escola',
+    templateUrl: './escola.component.html',
+    styleUrls: ['./escola.component.css']
+})
+export class EscolaComponent {
+    @Input() escola!: Escola;
+
+    // Restante do código do componente
+}
+
+
+
--- App-Fight-Finder-Projeto-TCC/src/app/tela-login/tela-login.component.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/tela-login/tela-login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Escola } from '../escola/escola.component';
 
 @Component({
   selector: 'app-tela-login',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class TelaLoginComponent {
 
+  @Input() escola: Escola | null = null; 
+
+escolaLogin!:Escola; 
+
+limparEscola():void {
+this.escolaLogin={
+  id:0,
+  nome: ' ', 
+  email:' ', 
+  senha:' ', 
+  foto:' ', 
+  treinadores: ' ',
+  alunos:' ', 
+  aulas:0,
 }
+
+}
+
+ngOnChanges(): void{
+  if (this.escola !=null ){
+  
+  this.escolaLogin={
+
+  id:this.escola.id, 
+  nome: this.escola.nome,
+  email: this.escola.email,
+  senha: this.escola.senha,
+  foto: this.escola.foto,
+  treinadores: this.escola.treinadores,
+  alunos: this.escola.alunos,
+  aulas:this.escola.aulas, 
+    }; 
+    console.log(this.escola.foto);
+  } else {
+    this.limparEscola(); 
+  }
+  }
+       
+
+}
+
