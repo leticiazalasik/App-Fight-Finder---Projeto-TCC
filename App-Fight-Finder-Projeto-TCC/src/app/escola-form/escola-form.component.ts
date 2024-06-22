a124c30e61270f5227e5310609e757bbc168be06
Letícia Zalasik <leticiazalasik@gmail.com>
2024-06-22 10:10:56 -0300

    :construction: Feat (escola-form): Mudanças no design.

--- App-Fight-Finder-Projeto-TCC/src/app/escola-form/escola-form.component.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/escola-form/escola-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Escola } from '../interfaces/Escola';
 
 @Component({
@@ -8,7 +8,13 @@ import { Escola } from '../interfaces/Escola';
 })
 export class EscolaFormComponent {
 
+  inserir: any;
+  exibirFormulario: string | undefined;
+
+
   @Input() escola: Escola | null = null; 
+  @Output() salvar: EventEmitter<any> = new EventEmitter<any>(); 
+@Output() editar: EventEmitter<any> = new EventEmitter<any>(); 
 
 escolaEditada!:Escola; 
 
@@ -47,5 +53,66 @@ ngOnChanges(): void{
   }
        
 
+salvarDados(): void{
+
+  this.salvar.emit(this.escolaEditada);
 }
 
+
+cancelar(): void {
+  this.limparEscola();
+}
+
+
+AdicionarDados(): void{
+  this.editar.emit(this.escolaEditada);
+}
+
+novoLutador(novaEscola: Escola): void {
+  this.exibirFormulario='novo';
+  
+}
+
+onSalvar(escola:Escola): void {
+  this.exibirFormulario=' '; 
+  
+  if (escola.id!=0){
+  this.editar.emit(escola);
+  } else {
+  this.inserir.emit(escola); 
+  }
+}
+
+onFileSelected(event: any) {
+  const file: File = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = this.handleReaderLoaded.bind(this);
+    reader.readAsArrayBuffer(file);
+
+    let fileNameElement = document.getElementById('file-name');
+    if (fileNameElement) {
+      fileNameElement.textContent = file.name;
+    }
+  } else {
+    // Limpe o nome do arquivo se nenhum arquivo for selecionado
+    let fileNameElement = document.getElementById('file-name');
+    if (fileNameElement) {
+      fileNameElement.textContent = 'Nenhum arquivo selecionado';
+    }
+  }
+  
+}
+
+handleReaderLoaded(readerEvt: ProgressEvent<FileReader>) {
+  if (readerEvt && readerEvt.target && readerEvt.target.result) {
+    let arrayBuffer = readerEvt.target.result as ArrayBuffer;
+    let binaryString = Array.from(new Uint8Array(arrayBuffer)).map(b => String.fromCharCode(b)).join('');
+    this.escolaEditada.foto = 'data:image/jpeg;base64,' + btoa(binaryString);
+    console.log(this.escolaEditada.foto);
+  }
+  
+}
+} 
