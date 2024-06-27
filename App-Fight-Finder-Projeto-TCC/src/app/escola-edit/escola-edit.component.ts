070cb70052356ca13f10dc63ad30147688b149e3
Letícia Zalasik <leticiazalasik@gmail.com>
2024-06-27 17:50:18 -0300

    :construction: Feat (app-escola-edit): Mudanças no app-escola-edit

--- App-Fight-Finder-Projeto-TCC/src/app/escola-edit/escola-edit.component.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/escola-edit/escola-edit.component.ts
@@ -9,6 +9,19 @@ export class EscolaEditComponent {
 
   mostrarFormulario = false;
 
+  onClickEditarPerfil() {
+    this.mostrarFormulario = !this.mostrarFormulario;
+    console.log('Valor de mostrarFormulario:', this.mostrarFormulario);
+}
+
+excluirConta() {
+  const confirmacao = confirm("Tem certeza de que deseja excluir sua conta permanentemente?\n \n Você irá perder todos os seus dados registrados.");
+  if (confirmacao) {
+      // Lógica para excluir a conta
+      alert("Conta excluída com sucesso!");
+ 
+  }
+}
 
 
 }
