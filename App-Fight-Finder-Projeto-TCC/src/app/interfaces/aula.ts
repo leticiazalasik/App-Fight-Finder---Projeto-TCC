28b483ac970187b3513342742755e441bb6c7621
Letícia Zalasik <leticiazalasik@gmail.com>
2024-06-28 17:02:03 -0300

    :construction: Feat (aula): Criação da interface aula

---
+++ App-Fight-Finder-Projeto-TCC/src/app/interfaces/aula.ts
@@ -0,0 +1,8 @@
+export interface Aula {
+    id: number;
+    data: string;
+    lutadoresPresentes: number[]; // Array de IDs de lutadores e treinadores
+    treinadoresPresentes: number[]; // Array de IDs de lutadores e treinadores
+    horario: number; 
+    
+    }
