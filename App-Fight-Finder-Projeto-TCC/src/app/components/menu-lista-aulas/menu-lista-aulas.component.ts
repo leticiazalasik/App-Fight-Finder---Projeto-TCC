0a1eae73fa7f547d5de7c419b7c5aa099e9facbd
Letícia Zalasik <leticiazalasik@gmail.com>
2024-07-15 13:05:00 -0300

    :bug: Feat (menu-lista-aulas): Criaçao do HTML, CSS e TS do componente

--- App-Fight-Finder-Projeto-TCC/src/app/components/menu-lista-aulas/menu-lista-aulas.component.ts
+++ App-Fight-Finder-Projeto-TCC/src/app/components/menu-lista-aulas/menu-lista-aulas.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Treinador } from '../../interfaces/Treinador';
 import { Lutador } from '../../interfaces/Lutador';
+import { Aula } from '../../interfaces/Aula';
 
 @Component({
   selector: 'app-menu-lista-aulas',
@@ -8,28 +9,53 @@ import { Lutador } from '../../interfaces/Lutador';
   styleUrl: './menu-lista-aulas.component.css'
 })
 export class MenuListaAulasComponent {
-
-  listaTreinadores: Treinador[] = [
+    public daysArray: number[] = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+      11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    ];
+  
+    public mesesArray: number[] = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+      11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    ];
+  
+    // Defina o ano inicial desejado (por exemplo, 2024)
+    private anoInicial: number = 2024;
+    private anoAtual: number = new Date().getFullYear();
+  
+    // Criando o array com os anos
+    public anosArray: number[] = [];
+  
+    constructor() {
+      for (let i: number = this.anoInicial; i <= this.anoAtual; i++) {
+        this.anosArray.push(i);
+      }
+    }
+  
+  listaaulas: Aula[] = [
     {
       id: 1,
-      nome: 'Antonio Valindolfo',
-    idade: 40,
-    modalidade: 'Taekwondo',
-    ativo: false,
-    foto: 'assets/img/perfil.png',
+      data: new Date(2024, 7, 15),
+       horario:"19",
+      lutadoresPresentes: [1],
+      treinadoresPresentes: [1],
+      lutadoresAusentes: [],
+      treinadoresAusentes: [],
     }
   ];
 
-  listaTreinadoresFiltro: Treinador[]= this.listaTreinadores; 
+  listaAulasFiltro: Aula[]= this.listaaulas; 
 
   @Output() filtro: EventEmitter<Boolean> = new EventEmitter<Boolean>();
-  @Output() listaLutadores: Lutador[] = [];
+  @Output() listaAulas: Aula[] = [];
 
   tela(tela?:Boolean): void { 
     this.filtro.emit(tela); 
   }
 
    exibirTodos():void{
-    this.listaTreinadoresFiltro=this.listaTreinadores;
+    this.listaAulasFiltro=this.listaaulas;
   }
 }
