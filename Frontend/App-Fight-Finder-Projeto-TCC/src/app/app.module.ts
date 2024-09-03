e5bba7f6fc92ca35cb837913527a65af4d2be3cf
Letícia Zalasik <leticiazalasik@gmail.com>
2024-09-03 18:51:01 -0300

    :heavy_minus_sign: Fix (presenca-lutadores-autonatica, menu-lista-lutadores...): Exclusão de componentes não mais utilizados, que foram incorporados a outros componentes.

--- Frontend/App-Fight-Finder-Projeto-TCC/src/app/app.module.ts
+++ Frontend/App-Fight-Finder-Projeto-TCC/src/app/app.module.ts
@@ -14,8 +14,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { MenunavComponent } from './components/menunav/menunav.component';
 import { MenuInicialComponent } from './components/menu-inicial/menu-inicial.component';
 import { MenuLutadoresComponent } from './components/menu-lutadores/menu-lutadores.component';
-import { MenuListaLutadoresComponent } from './components/menu-lista-lutadores/menu-lista-lutadores.component';
-import { TelaLutadoresComponent } from './components/tela-lutadores/tela-lutadores.component';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 import { ComentarioComponent } from './components/comentario/comentario.component';
 import { AdicionarComentarioComponent } from './components/adicionar-comentario/adicionar-comentario.component';
@@ -29,20 +27,15 @@ import { EscolaEditComponent } from './components/escola-edit/escola-edit.compon
 import { AulaComponent } from './components/aula/aula.component';
 import { AulaFormComponent } from './components/aula-form/aula-form.component';
 import { PresencaLutadoresManualComponent } from './components/presenca-lutadores-manual/presenca-lutadores-manual.component';
-import { PresencaLutadoresAutomaticaComponent } from './components/presenca-lutadores-automatica/presenca-lutadores-automatica.component';
 import { PresencaTreinadoresManualComponent } from './components/presenca-treinadores-manual/presenca-treinadores-manual.component';
 import { TreinadorComponent } from './components/treinador/treinador.component';
 import { TreinadorListComponent } from './components/treinador-list/treinador-list.component';
 import { TreinadorFormComponent } from './components/treinador-form/treinador-form.component';
 import { TreinadorEditComponent } from './components/treinador-edit/treinador-edit.component';
-import { TelaTreinadoresComponent } from './components/tela-treinadores/tela-treinadores.component';
-import { MenuListaTreinadoresComponent } from './components/menu-lista-treinadores/menu-lista-treinadores.component';
-import { MenuTreinadoresComponent } from './components/menu-treinadores/menu-treinadores.component';
 import { AulaListComponent } from './components/aula-list/aula-list.component';
 import { MenuListaAulasComponent } from './components/menu-lista-aulas/menu-lista-aulas.component';
 import { AulaEditComponent } from './components/aula-edit/aula-edit.component';
 import { TelaAulasComponent } from './components/tela-aulas/tela-aulas.component';
-import { MenuAulasComponent } from './components/menu-aulas/menu-aulas.component';
 import { VisualizacaoLutadorComponent } from './components/visualizacao-lutador/visualizacao-lutador.component';
 import { LutadorFormCompleteComponent } from './components/lutador-form-complete/lutador-form-complete.component';
 import { EsqueciSenhaComponent } from './components/esqueci-senha/esqueci-senha.component';
@@ -63,7 +56,7 @@ import { EditarComentarioComponent } from './components/editar-comentario/editar
     LutadorComponent,
     LutadorFormComponent, 
     LutadorListComponent, 
-    LutadorEditComponent, FooterComponent, HeaderComponent, MenunavComponent, MenuInicialComponent, MenuLutadoresComponent, MenuListaLutadoresComponent, TelaLutadoresComponent, SplashScreenComponent, ComentarioComponent, AdicionarComentarioComponent, TelaLoginComponent, EscolaComponent, EscolaFormComponent, BoasVindasComponent, MenuCriarContaComponent, AjudaComponent, EscolaEditComponent, AulaComponent, AulaFormComponent, PresencaLutadoresManualComponent, PresencaLutadoresAutomaticaComponent, PresencaTreinadoresManualComponent, TreinadorComponent, TreinadorListComponent, TreinadorFormComponent, TreinadorEditComponent, TelaTreinadoresComponent, MenuListaTreinadoresComponent, MenuTreinadoresComponent, AulaListComponent, MenuListaAulasComponent, AulaEditComponent, TelaAulasComponent, MenuAulasComponent,  VisualizacaoLutadorComponent, LutadorFormCompleteComponent, EsqueciSenhaComponent, GerarLutaComponent, ResultadoLutasComponent, VisualizacaoTreinadorComponent, AulasListComponent, PresentesAulaComponent, AusentesAulasComponent, EditarComentarioComponent
+    LutadorEditComponent, FooterComponent, HeaderComponent, MenunavComponent, MenuInicialComponent, MenuLutadoresComponent, SplashScreenComponent, ComentarioComponent, AdicionarComentarioComponent, TelaLoginComponent, EscolaComponent, EscolaFormComponent, BoasVindasComponent, MenuCriarContaComponent, AjudaComponent, EscolaEditComponent, AulaComponent, AulaFormComponent, PresencaLutadoresManualComponent, PresencaTreinadoresManualComponent, TreinadorComponent, TreinadorListComponent, TreinadorFormComponent, TreinadorEditComponent, AulaListComponent, MenuListaAulasComponent, AulaEditComponent, TelaAulasComponent, VisualizacaoLutadorComponent, LutadorFormCompleteComponent, EsqueciSenhaComponent, GerarLutaComponent, ResultadoLutasComponent, VisualizacaoTreinadorComponent, AulasListComponent, PresentesAulaComponent, AusentesAulasComponent, EditarComentarioComponent
   ],
   imports: [
     BrowserModule,
