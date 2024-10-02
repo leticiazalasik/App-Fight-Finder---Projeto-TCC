cc4fb538e8db3d1578ee9695816f8ab3b25b52db
Letícia Zalasik <leticiazalasik@gmail.com>
2024-10-01 21:32:05 -0300

    :sparkles: Feat (telas-unicas e telas-compartilhadas): Mudanças de HTML, CSS e TS.

--- Frontend/App-Fight-Finder-Projeto-TCC/src/app/components/interfaces/Luta.ts
+++ Frontend/App-Fight-Finder-Projeto-TCC/src/app/components/interfaces/Luta.ts
@@ -3,11 +3,9 @@ import { Treinador } from "./Treinador";
 
 export interface Luta{
     id: number; 
-    rodada: number; 
     status: boolean;
     treinador: Treinador; 
     lutador1: Lutador; 
     lutador2: Lutador; 
-    totalLutas: number; 
     
 }
