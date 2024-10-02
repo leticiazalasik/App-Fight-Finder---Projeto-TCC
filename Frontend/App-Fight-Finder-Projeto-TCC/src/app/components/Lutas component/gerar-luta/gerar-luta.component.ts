eceedbb13fa1f16d1169d6ae625a0b45afd996b8
Letícia Zalasik <leticiazalasik@gmail.com>
2024-10-01 21:08:34 -0300

    :sparkles: Feat (lutas): Mudanças de front e funções.

--- Frontend/App-Fight-Finder-Projeto-TCC/src/app/components/Lutas component/gerar-luta/gerar-luta.component.ts
+++ Frontend/App-Fight-Finder-Projeto-TCC/src/app/components/Lutas component/gerar-luta/gerar-luta.component.ts
@@ -17,9 +17,9 @@ import { TreinadorService } from '../../../servicos/treinador.service';
   styleUrl: './gerar-luta.component.css'
 })
 export class GerarLutaComponent {
-listaTreinadores!: Treinador[];
-listaFiltradaLutadores!: Lutador[];
-listaFiltradaTreinadores!: Treinador[];
+  listaTreinadores!: Treinador[];
+  listaFiltradaLutadores!: Lutador[];
+  listaFiltradaTreinadores!: Treinador[];
 
   aulaService: any;
   aula: any;
@@ -29,69 +29,72 @@ listaFiltradaTreinadores!: Treinador[];
   listaTreinadorAula: TreinadorAula[] = [];
   isEdicao: boolean = false;
 
+  varNumero: number = 0;
+
+
   constructor(
-    private lutaService: LutaService, 
+    private lutaService: LutaService,
     private router: Router,
     private route: ActivatedRoute,
     private lutadorService: LutadorService,
     private treinadorService: TreinadorService
-  ){}
+  ) { }
 
-  @Input() luta: Luta | null = null; 
+  @Input() luta: Luta | null = null;
   exibirFormulario: string | undefined;
 
-treinador!: Treinador;
-lutador1!: Lutador; 
-lutador2!:Lutador; 
-treinadoresPresentes!: Treinador[]; 
-lutadoresPresentes!: Lutador[]; 
-isGenero!: Boolean; 
-idade!: number; 
-faixa!:Boolean; 
+  treinador!: Treinador;
+  lutador1!: Lutador;
+  lutador2!: Lutador;
+  treinadoresPresentes!: Treinador[];
+  lutadoresPresentes!: Lutador[];
 
-treinadores!: Treinador; 
-lutadores!: Lutador; 
+  varIsGenero!: Boolean;
+  varIdade!: number;
+  varFaixa!: Boolean;
+  varPeso!: number;
+  varAltura!: number;
+  varIsColorida!: Boolean;
 
-listaLutadores:Lutador[]=[];
+  treinadores!: Treinador;
+  lutadores!: Lutador;
+
+  listaLutadores: Lutador[] = [];
 
   filtroSelecionado: string = '';
   lutadoresSelecionados: number[] = []; // IDs dos lutadores selecionados
   treinadoresSelecionados: number[] = []; // IDs dos lutadores selecionados
 
-  lutaEditada!: Luta; 
-  limparLuta():void {
-    this.lutaEditada={
+  lutaEditada!: Luta;
+  limparLuta(): void {
+    this.lutaEditada = {
       id: 0,
-      rodada: 0,
       status: false,
       treinador: this.treinador,
       lutador1: this.lutador1,
-      lutador2: this.lutador1, 
-      totalLutas: 0
-    }
+      lutador2: this.lutador1
+        }
   }
-  
-  
-  ngOnChanges(): void{
-    if (this.treinador !=null ){
-    
-      this.lutaEditada={
+
+
+  ngOnChanges(): void {
+    if (this.treinador != null) {
+
+      this.lutaEditada = {
         id: this.lutaEditada.id,
-        rodada: this.lutaEditada.rodada,
         status: this.lutaEditada.status,
         treinador: this.lutaEditada.treinador,
         lutador1: this.lutaEditada.lutador1,
         lutador2: this.lutaEditada.lutador2,
-        totalLutas: this.lutaEditada.totalLutas
-      }; 
+      };
     } else {
-      this.limparLuta(); 
+      this.limparLuta();
     }
-    }
-  
-    ngOnInit():void{ 
+  }
 
-      const id = Number(this.route.snapshot.paramMap.get('id'));
+  ngOnInit(): void {
+
+    const id = Number(this.route.snapshot.paramMap.get('id'));
     if (id && id != 0) {
       this.isEdicao = true;
       this.aulaService.findById(id).subscribe((data: any) => {
@@ -111,75 +114,75 @@ listaLutadores:Lutador[]=[];
       this.listaFiltradaTreinadores = this.listaTreinadores; // Exibe todos ao carregar a página
       this.criarListaTreinadorAula(this.listaTreinadores);
     });
-  
-      this.limparLuta();
-      }
-  
 
-      criarListaLutadorAula(listaLutadores:Lutador[]){
-        listaLutadores.forEach(lutador => {
-          const LutadorAula:LutadorAula={
-            id: undefined,
-            presente: true,
-            lutador: lutador,
-            aulaLutador: undefined
-          }; 
-         this.listaLutadorAula.push(LutadorAula); 
-    
-        });
-      }
-    
-      criarListaTreinadorAula(listaTreinadores:Treinador[]){
-        listaTreinadores.forEach(treinador => {
-          const TreinadorAula:TreinadorAula={
-            id: undefined,
-            presente: true,
-            treinador: treinador,
-            aulaTreinador: undefined
-          }; 
-         this.listaTreinadorAula.push(TreinadorAula); 
-    
-        });
-      }
-  
-  salvarDados(): void{
+    this.limparLuta();
+  }
+
+
+  criarListaLutadorAula(listaLutadores: Lutador[]) {
+    listaLutadores.forEach(lutador => {
+      const LutadorAula: LutadorAula = {
+        id: undefined,
+        presente: true,
+        lutador: lutador,
+        aulaLutador: undefined
+      };
+      this.listaLutadorAula.push(LutadorAula);
+
+    });
+  }
+
+  criarListaTreinadorAula(listaTreinadores: Treinador[]) {
+    listaTreinadores.forEach(treinador => {
+      const TreinadorAula: TreinadorAula = {
+        id: undefined,
+        presente: true,
+        treinador: treinador,
+        aulaTreinador: undefined
+      };
+      this.listaTreinadorAula.push(TreinadorAula);
+
+    });
+  }
+
+  salvarDados(): void {
     this.lutaService.add(this.lutaEditada).subscribe(() => {
       this.router.navigate(['/menuInicial']);
     });
   }
-  
-  
+
+
   cancelar(): void {
     this.limparLuta();
   }
-  
-  
+
+
 
   filtrarAulas() {
-  this.aulaService.findAll().subscribe((aulas: any[]) => {
-    this.aulasAtivas = aulas.filter(aula => aula.status === true);
-    
-    // Agora que você tem a lista de aulas ativas, pode pegar a primeira ou alguma específica
-    if (this.aulasAtivas.length > 0) {
-      this.aulaAtiva = this.aulasAtivas[0]; // Exemplo: pegando a primeira aula ativa
-      this.aula.hora = this.aulaAtiva.hora; // Atualizando a hora da aula ativa
-    }
-  });
-}
+    this.aulaService.findAll().subscribe((aulas: any[]) => {
+      this.aulasAtivas = aulas.filter(aula => aula.status === true);
+
+      // Agora que você tem a lista de aulas ativas, pode pegar a primeira ou alguma específica
+      if (this.aulasAtivas.length > 0) {
+        this.aulaAtiva = this.aulasAtivas[0]; // Exemplo: pegando a primeira aula ativa
+        this.aula.hora = this.aulaAtiva.hora; // Atualizando a hora da aula ativa
+      }
+    });
+  }
   // Método para filtrar a lista
   filtrarLutadores() {
     // Primeiro, encontre a aula ativa baseada no horário atual ou qualquer outra lógica
     this.aulaService.getAulaAtiva().subscribe((aulaAtiva: any) => {
       if (aulaAtiva) {
         this.aula = aulaAtiva; // Definindo a aula ativa para poder acessar sua hora
-  
+
         // Agora que temos a aula ativa, aplicamos os filtros
         if (this.filtroSelecionado === 'alunosDaTurma') {
           // Filtra os lutadores que têm a mesma turma (horário) da aula ativa
           this.listaFiltradaLutadores = this.listaLutadores.filter(
             lutador => lutador.turma === this.aula.hora
           );
-  
+
           // Seleciona todos os lutadores filtrados por padrão
           this.lutadoresSelecionados = this.listaFiltradaLutadores.map(
             lutador => lutador.id
@@ -203,14 +206,14 @@ listaLutadores:Lutador[]=[];
     this.aulaService.getAulaAtiva().subscribe((aulaAtiva: any) => {
       if (aulaAtiva) {
         this.aula = aulaAtiva; // Definindo a aula ativa para poder acessar sua hora
-  
+
         // Agora que temos a aula ativa, aplicamos os filtros
         if (this.filtroSelecionado === 'alunosDaTurma') {
           // Filtra os lutadores que têm a mesma turma (horário) da aula ativa
           this.listaFiltradaLutadores = this.listaLutadores.filter(
             lutador => lutador.turma === this.aula.hora
           );
-  
+
           // Seleciona todos os lutadores filtrados por padrão
           this.lutadoresSelecionados = this.listaFiltradaLutadores.map(
             lutador => lutador.id
@@ -242,5 +245,45 @@ listaLutadores:Lutador[]=[];
       this.treinadoresSelecionados = this.treinadoresSelecionados.filter(id => id !== treinadorAula.treinador.id);
     }
   }
+
+
+  // salvar(): void {
+  //   this.lutaService.add().subscribe(() => {
+  //     this.router.navigate(['/luta']);
+  //   });
+  // }
+
+  // listaLutadorLuta: LutadorAula[]= this.listaLutadorAula
+  // listaTreinadorLuta: TreinadorAula[]= this.listaTreinadorAula
+
+  // gerarLutas(): void{ 
+  //   while (this.listaTreinadorLuta.length > 0) {
+  //     const treinador = this.treinadores.pop(); // Pega um treinador da lista
+  //     if (!treinador) break; // Se não houver mais treinadores, saia do loop
   
+  //     const lutador1 = getRandomLutador(this.listaLutadorLuta); // Pega um lutador aleatório
+  //     const lutador2 = getRandomLutador(this.listaLutadorLuta); // Pega outro lutador aleatório
+  
+  //     // Crie a luta com os lutadores escolhidos
+  //     const novaLuta: Lutador[] = [lutador1, lutador2];
+  //     console.log(`Nova luta: ${lutador1.nome} vs ${lutador2.nome}`);
+  
+  //     // Atualize o status dos lutadores para true
+  //     lutador1.status = true;
+  //     lutador2.status = true;
+  //   }
+  // }
+  
+  // // Função auxiliar para pegar um lutador aleatório
+  // function getRandomLutador(lutadores: Lutador[]): Lutador {
+  //   const randomIndex = Math.floor(Math.random() * lutadores.length);
+  //   return lutadores.splice(randomIndex, 1)[0];
+  // }
+  
+  // // Exemplo de uso
+  // criarLutasAleatorias(treinadores, lutadores);
+
+   
 }
+
+
